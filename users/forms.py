ab7a06d5effac17db330b93440ff638cb2f91c06
Ingyu Yang <79055280+igoo-Y@users.noreply.github.com>
2021-07-09 19:24:02 +0900

    User SignUp

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,7 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from django.forms import models
+from django.forms.widgets import PasswordInput
 from . import models
 
 
@@ -19,3 +21,38 @@ class LoginForm(forms.Form):
                 self.add_error("password", forms.ValidationError("Password is wrong"))
         except models.User.DoesNotExist:
             self.add_error("email", forms.ValidationError("User does not exist"))
+
+
+class SignUpForm(forms.Form):
+
+    email = forms.EmailField()
+    first_name = forms.CharField(max_length=80)
+    last_name = forms.CharField(max_length=80)
+    password = forms.CharField(widget=PasswordInput)
+    password1 = forms.CharField(widget=PasswordInput, label="Confirm Password")
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        try:
+            models.User.objects.get(email=email)
+            raise forms.ValidationError("User already exists with that email.")
+        except models.User.DoesNotExist:
+            return email
+
+    def clean_password1(self):
+        password = self.cleaned_data.get("password")
+        password1 = self.cleaned_data.get("password1")
+        if password != password1:
+            raise forms.ValidationError("Password confirmation does not match")
+        else:
+            return password
+
+    def save(self):
+        email = self.cleaned_data.get("email")
+        first_name = self.cleaned_data.get("first_name")
+        last_name = self.cleaned_data.get("last_name")
+        password = self.cleaned_data.get("password")
+        user = models.User.objects.create_user(email, email, password)
+        user.first_name = first_name
+        user.last_name = last_name
+        user.save()
--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,5 @@ app_name = "users"
 urlpatterns = [
     path("users/login", views.LoginView.as_view(), name="login"),
     path("users/logout", views.log_out, name="logout"),
+    path("users/signup", views.SignUpView.as_view(), name="signup"),
 ]
