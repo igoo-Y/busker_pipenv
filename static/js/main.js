8f2d1201e0bd54048fde15da7834295198d2de9a
Ingyu Yang <79055280+igoo-Y@users.noreply.github.com>
2021-07-10 17:41:16 +0900

    webSocket

--- static/js/main.js
+++ static/js/main.js
@@ -9,9 +9,20 @@ var webSocket;
 
 function webSocketOnMessage(event){
     var parsedData = JSON.parse(event.data);
-    var message = parsedData['message'];
+    var peerUsername = parsedData['peer'];
+    var action = parsedData['action'];
 
-    console.log('message', message)
+    if(username == peerUsername){
+        return;
+    }
+
+    var receiver_channel_name = parsedData['message']['receiver_channel_name'];
+
+    if(action == 'new-peer'){
+        createOfferer(peerUsername, receiver_channel_name);
+
+        return;
+    }
 }
 
 btnJoin.addEventListener('click', () => {
@@ -47,13 +58,107 @@ btnJoin.addEventListener('click', () => {
     webSocket = new WebSocket(endPoint);
 
     webSocket.addEventListener('open', (e) => {
-        console.log('Connection Opened!')
+        console.log('Connection Opened!');
+
+        sendSignal('new-peer', {});
     });
     webSocket.addEventListener('message', webSocketOnMessage);
     webSocket.addEventListener('close', (e) => {
-        console.log('Connection Closed!')
+        console.log('Connection Closed!');
     });
     webSocket.addEventListener('error', (e) => {
-        console.log('Error Occured!')
+        console.log('Error Occured!');
     });
 });
+
+var localStream = new MediaStream();
+
+const constraints = {
+    'video': true,
+    'audio': true
+};
+
+const localVideo = document.querySelector('#local-video');
+
+var userMedia = navigator.mediaDevices.getUserMedia(constraints)
+    .then(stream => {
+        localStream = stream;
+        localVideo.srcObject = localStream;
+        localVideo.muted = true;
+    })
+    .catch(error => {
+        console.log('Error accessing media devices.', error);
+    });
+
+    
+
+function sendSignal(action, message){
+    var jsonStr = JSON.stringify({
+        'peer': username,
+        'action': action,
+        'message': message,
+    });
+
+    webSocket.send(jsonStr);
+}
+
+function createOfferer(peerUsername, receiver_channel_name){
+    var peer = new RTCPeerConnection(null);
+
+    addLocalTracks(peer);
+
+    var dc = peer.createDataChannel('channel');
+    dc.addEventListener('open', () => {
+        console.log('Connection Opened!');
+    });
+    dc.addEventListener('message', dcOnMessage)
+
+    var remoteVideo = createVideo(peerUsername);
+    setOnTrack(peer, peerUsername);
+}
+
+function addLocalTracks(peer){
+    localStream.getTracks().forEach(track => {
+        peer.addTrack(track, localStream);
+    });
+
+    return;
+}
+
+var messageList = document.querySelector('#message-list');
+
+function dcOnMessage(event){
+    var message = evnet.data;
+
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode(message));
+    messageList.appendChild(li);
+}
+
+function createVideo(peerUsername){
+    var videoContainer = document.querySelector('#video-container');
+
+    var remoteVideo = document.createElement('video');
+
+    remoteVideo.id = peerUsername + '-video';
+    remoteVideo.autoplay = true;
+    remoteVideo.playsInline = true;
+
+    var videoWrapper = document.createElement('div');
+
+    videoContainer.appendChild(videoWrapper);
+
+    videoWrapper.appendChild(remoteVideo);
+
+    return remoteVideo;
+}
+
+function setOnTrack(peer, remoteVideo){
+    var remoteStream = new MediaStream();
+
+    remoteVideo.srcObject = remoteStream;
+
+    peer.addEventListener('track', async (event) => {
+        remoteStream.addTrack(event.track, remoteStream);
+    });
+}
