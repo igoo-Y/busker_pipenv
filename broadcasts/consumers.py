e5f5b6539c88ed73ec7cf9fd7b373549677c617e
Ingyu Yang <79055280+igoo-Y@users.noreply.github.com>
2021-07-20 11:49:26 +0900

    Video

--- broadcasts/consumers.py
+++ broadcasts/consumers.py
@@ -19,6 +19,19 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
     async def receive(self, text_data):
         receive_dict = json.loads(text_data)
         message = receive_dict["message"]
+        action = receive_dict["action"]
+
+        if (action == "new-offer") or (action == "new-answer"):
+            receiver_channel_name = receive_dict["message"][
+                "receiver_channel_name"
+            ] = self.channel_name
+
+            await self.channel_layer.send(
+                receiver_channel_name,
+                {"type": "send.sdp", "receive_dict": receive_dict},
+            )
+
+            return
 
         receive_dict["message"]["receiver_channel_name"] = self.channel_name
 
