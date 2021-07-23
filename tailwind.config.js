deb26906d71add26e13ece693e94dc4c93a238fc
nadoyeon <ehdus6223@naver.com>
2021-07-23 15:35:28 +0900

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,8 @@
-const colors = require('tailwindcss/colors')
-
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      colors: {
-        'busker-navy': '#1D1D43',
-      },
-      spacing: {
-        "25vh": "25vh",
-        "50vh": "50vh",
-        "75vh": "75vh",
-      }
-    },
+    extend: {},
   },
   variants: {
     extend: {},
