447e7f92c0229bad29f32596fc212f1131438e49
nadoyeon <ehdus6223@naver.com>
2021-08-04 16:43:25 +0900

    google naver

--- config/settings.py
+++ config/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.naver'
 ]
 
 MIDDLEWARE = [
