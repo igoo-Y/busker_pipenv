90617b662986970be6cc37d1a815fcaf548a18a9
Ingyu Yang <79055280+igoo-Y@users.noreply.github.com>
2021-08-06 18:59:29 +0900

    8/6

--- users/urls.py
+++ users/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path("login/", views.LoginView.as_view(), name="login"),
     path("login/kakao/", views.kakao_login, name="kakao_login"),
     path("login/kakao/callback/", views.kakao_callback, name="kakao_callback"),
-    path("login/naver/", views.naver_login, name="naver_login"),
     path("logout/", views.log_out, name="logout"),
     path("signup/", views.SignUpView.as_view(), name="signup"),
     path("<int:pk>/", views.UserProfileView.as_view(), name="profile"),
