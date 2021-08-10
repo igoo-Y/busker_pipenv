312ebe1fae2f8d8dc423a956a318fc53c69ec55d
Ingyu Yang <79055280+igoo-Y@users.noreply.github.com>
2021-08-10 18:18:25 +0900

    Revert "studio crud"
    This reverts commit 7c65838f5637c87b781604bebe75e4039f560119.
    
    revert 1hour ago

--- studios/forms.py
+++ studios/forms.py
@@ -1,21 +1,33 @@
 from django import forms
+<<<<<<< HEAD
+from django.forms import models
+=======
 from django.forms import fields
+>>>>>>> ingyu
 from . import models
 
 
 class CreateStudioForm(forms.ModelForm):
     class Meta:
         model = models.Studio
+<<<<<<< HEAD
+        fields = ("name",)
+=======
         fields = (
             "name",
             "desc",
             "image",
         )
+>>>>>>> ingyu
 
     def save(self, *args, **kwargs):
         studio = super().save(commit=False)
         return studio
 
 
+<<<<<<< HEAD
+class UpdateBroadcastForm(forms.Form):
+=======
 class UpdateStudioForm(forms.Form):
+>>>>>>> ingyu
     pass
