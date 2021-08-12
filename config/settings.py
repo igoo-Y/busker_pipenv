819a5d91da6d548dede0eb3b5827e3e50ef19df1
Ingyu Yang <79055280+igoo-Y@users.noreply.github.com>
2021-08-12 19:37:39 +0900

    allauth

--- config/settings.py
+++ config/settings.py
@@ -158,8 +158,8 @@ AUTHENTICATION_BACKENDS = (
     "allauth.account.auth_backends.AuthenticationBackend",
 )
 
-LOGIN_REDIRECT_URL = "index"  # 로그인 후 리디렉션할 페이지
-ACCOUNT_LOGOUT_REDIRECT_URL = "index"  # 로그아웃 후 리디렉션 할 페이지
+LOGIN_REDIRECT_URL = "/"  # 로그인 후 리디렉션할 페이지
+ACCOUNT_LOGOUT_REDIRECT_URL = "/"  # 로그아웃 후 리디렉션 할 페이지
 ACCOUNT_LOGOUT_ON_GET = True  # 로그아웃 버튼 클릭 시 자동 로그아웃A
 
 SITE_ID = 1
